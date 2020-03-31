e4de59f0bd6cb1cfe0dea0fe975e5d6a341b5ca9
janthenelli <57114460+janthenelli@users.noreply.github.com>
2020-03-30 23:12:23 -0400

    fixed search functionality

--- js/main.js
+++ js/main.js
@@ -4,13 +4,11 @@
 const searchBar = document.querySelector('input');
 searchBar.addEventListener('keyup', function(event) {
     const searchTerm = event.target.value.toLowerCase();
-    const imgs = document.querySelector('a').getElementsByTagName('data-title');
-    Array.from(imgs).forEach(function(img) {
-        const description = img.textContent;
-        if (description.toLowerCase().indexOf(seacrhTerm) != -1) {
-            img.style.display = 'block';
-        } else {
-            img.style.display = 'none';
-        }
-    });
+    if (searchTerm === '') {
+        $('.thumbnail').removeClass('hidden');
+        return;
+    }
+    const imgs = $('.thumbnail a[data-title*="' + searchTerm + '"]');
+    $('.thumbnail').addClass('hidden');
+    imgs.parent().removeClass('hidden');
 });
